74451af601dc58ee08e1fde5b4a5715e1fe66d5b
joel vilca Alania <joel@idw.com.pe>
2019-02-07 16:55:59 -0500

    initial commit goplay

---
+++ src/components/Menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-toolbar color="#546E7A">
+    <v-toolbar-side-icon></v-toolbar-side-icon>
+    <v-toolbar-title>GoPlay</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn color="white" flat>Inicio</v-btn>
+      <v-btn color="white" flat>Suscribete</v-btn>
+      <v-btn color="white" flat>Gold</v-btn>
+      <v-btn color="white" flat>Elite</v-btn>
+      <v-btn color="white" flat>Creditos</v-btn>
+      <v-btn color="white" flat>Iniciar Sesion</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+.theme--light.v-toolbar {
+    color: white;
+}
+</style>
+
+
