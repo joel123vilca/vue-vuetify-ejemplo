95808f7f142425adf259270af4157f11cece40c6
joel vilca Alania <joel@idw.com.pe>
2019-02-16 14:40:24 -0500

    modificacion en inicio

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -5,14 +5,15 @@
     <v-carousel-item
       v-for="(item,i) in items"
       :key="i"
+      :src="item.src"
       color="white darken-4"
       style="height:100vh"
     >
        <v-layout class="texto" align-end justify-center column fill-height >
          <v-flex md7 offset-md5>
-        <h3 class="titulo">{{item.title}}</h3>
-        <p class="descripcion" >Lorem Ipsum is simply dummy textof the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-        <h3 class="display-3 black--text">{{item.precio}}</h3>
+        <h3 class="titulo white--text">{{item.title}}</h3>
+        <p class="descripcion white--text" >Lorem Ipsum is simply dummy textof the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+        <h3 class="display-3 white--text">{{item.precio}}</h3>
         <v-btn
             large
             round 
@@ -35,14 +36,17 @@
           {
             title:'Membresia Elite',
             precio:'24/7',
+            src: 'http://www.bejarfm.es/wp-content/uploads/2018/07/Apuestas.jpg'
           },
           {
             title:'Membresia Gold',
             precio:'24/7',
+            src:'http://www.bejarfm.es/wp-content/uploads/2018/07/Apuestas.jpg'
           },
           {
             title:'Membresia Premium',
             precio:'24/7',
+            src:'http://www.bejarfm.es/wp-content/uploads/2018/07/Apuestas.jpg'
           }
         ]
       }
@@ -54,14 +58,14 @@
   margin-right: 100px;
 }
 .titulo{
-  font-weight:black;
+  font-weight:white;
   font-size: 4em;
   display:6em;
-  color:black
+  color:white;
 }
 .descripcion{
   font-size:1.5em;
-  color:black;
+  color:white;
 }
 @media screen and (max-width: 480px) {
   .texto{
@@ -69,14 +73,14 @@
   margin-top: 40%;
 }
 .titulo{
-  font-weight:black;
+  font-weight:white;
   font-size: 2em;
   display:1em;
-  color:black;
+  color:white;
 }
 .descripcion{
   font-size: 1.3em;
-  color:black;
+  color:white;
 }
 }
 </style>
